0e677c45374060fd48842dfaf984251717ca8753
Dilshan-Mindika <maxdilshan666@gmail.com>
2025-05-19 08:24:35 +0530

    Route reating bug fixed

--- app/Models/Route.php
+++ app/Models/Route.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Route extends Model
 {
+    // Add 'city' to the $fillable array to allow mass assignment
+    protected $fillable = ['city', 'description'];
+    
     function clients() {
         return $this->hasMany(Client::class);
     }
